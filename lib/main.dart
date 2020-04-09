bbd2422d7e2f33f426b8efc7e53b04b659ccda1b
aaronkwok111 <49882438+aaronkwok111@users.noreply.github.com>
2020-04-09 10:43:09 -0700

    Create Home screen with percent indicators linked to shared preference date values

--- lib/main.dart
+++ lib/main.dart
@@ -60,3 +60,106 @@ class SplashState extends State<Splash> {
     );
   }
 }
+class Home extends StatelessWidget {
+
+  DateTime selectedDate;
+  Home(selectedDate)
+  {
+    this.selectedDate = selectedDate;
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Column(
+          children:<Widget>[
+            Container(
+                padding:EdgeInsets.fromLTRB(0,20,0,0),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Hello!",
+                        style: TextStyle(fontSize:40))
+                )
+            ),
+            Container(
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Nice to see you",
+                        style: TextStyle(fontSize:25))
+                )
+            ),
+            Container(
+              width:125,
+              height:125.0,
+              child: FloatingActionButton(
+                child: Text('Track My Contacts',
+                    style: TextStyle(fontSize: 10.0)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => config()),
+                  );
+                },
+              ),
+            ),
+            Container(
+              child:CircularPercentIndicator(
+                radius: 250.0,
+                lineWidth: 13.0,
+                animation: true,
+                percent: .7, //(selectedDate.difference(DateTime.now()).inDays/30).abs(),
+                center:
+                new Text(
+                  '${selectedDate.difference(DateTime.now()).inDays.abs()}',
+                  style:
+                  new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                circularStrokeCap: CircularStrokeCap.round,
+                progressColor: Colors.blue,
+              ),
+            ),
+            Text(
+                'hello'
+            ),
+
+            Row(
+                children: <Widget>[
+                  Container(
+                    child:CircularPercentIndicator(
+                      radius: 150.0,
+                      lineWidth: 13.0,
+                      animation: true,
+                      percent: 0.7,
+                      center: new Text(
+                        "70.0%",
+                        style:
+                        new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      progressColor: Colors.blue,
+                    ),
+                  ),
+                  Container(
+                    child:CircularPercentIndicator(
+                      radius: 150.0,
+                      lineWidth: 13.0,
+                      animation: true,
+                      percent: 0.7,
+                      center: new Text(
+                        "70.0%",
+                        style:
+                        new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      progressColor: Colors.blue,
+                    ),
+                  )
+                ]
+            )
+          ]
+      ),
+    );
+  }
+}
