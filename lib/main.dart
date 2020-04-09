a55507314a4a25d9d4e1390db4831ab097d1919a
aaronkwok111 <49882438+aaronkwok111@users.noreply.github.com>
2020-04-09 10:46:11 -0700

    Code cleanup 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'dart:async';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
-//import 'package:custom_radio/custom_radio.dart';
+
 
 void main() => runApp(new MyApp());
 
